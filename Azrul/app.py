970cad067a551eae362a7f6e286005ea7be72e1d
azrul <Changminidol>
2023-06-29 00:08:16 +0800

    change

--- Azrul/app.py
+++ Azrul/app.py
@@ -1,1 +1,10 @@
-print("hello")#hello
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run()
