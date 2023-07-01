008e77c773e056b187dfd302ee16221db1417c41
azrul <Changminidol>
2023-07-02 01:06:12 +0800

    change

--- Azrul/app.py
+++ Azrul/app.py
@@ -1,16 +1,19 @@
-from flask import Flask,flash, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,flash
+import os
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    image_url = '/static/img/shoes.jpg'
-    return render_template("home.html",image_url=image_url)
 
-# @app.route('/', method=['POST'])
-# def upload_image():
-#     if 'file' not in request.files:
-#         flash('No file part')
-#         return redirect(request.url)
+imagefolder = os.path.join('static','image')
+
+app.config['UPLOAD_FOLDER'] = imagefolder
+
+@app.route('/')
+
+def home():
+    img1 = os.path.join(app.config['UPLOAD_FOLDER'], 'shoes.jpg')
+    return render_template("home.html", user_image=img1)
+
+
 
 if __name__ == '__main__':
     app.run()
