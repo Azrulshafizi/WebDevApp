12866b478e5bc6269cb6bfa7fa87ea63c31c24d7
azrul <Changminidol>
2023-07-02 18:39:35 +0800

    change

--- Azrul/app.py
+++ Azrul/app.py
@@ -3,15 +3,18 @@ import os
 
 app = Flask(__name__)
 
-imagefolder = os.path.join('static','image')
+image_folder = os.path.join('static','image')
 
-app.config['UPLOAD_FOLDER'] = imagefolder
+app.config['UPLOAD_FOLDER'] = image_folder
 
 @app.route('/')
 
 def home():
-    img1 = os.path.join(app.config['UPLOAD_FOLDER'], 'shoes.jpg')
-    return render_template("home.html", user_image=img1)
+    img = os.path.join(app.config['UPLOAD_FOLDER'], 'shoes.png')
+    print(img)
+    return render_template("home.html", user_image=img)
+
+
 
 
 
