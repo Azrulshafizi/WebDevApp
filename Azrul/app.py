acb81203b06fe147b87af429749f12b7e887bc66
azrul <Changminidol>
2023-07-18 16:38:10 +0800

    change

--- Azrul/app.py
+++ Azrul/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for,flash
 import os
-
+from form import CreateUserForm
+import shelve, member
 app = Flask(__name__)
 
 image_folder = os.path.join('static','image')
@@ -11,10 +12,37 @@ app.config['UPLOAD_FOLDER'] = image_folder
 
 def home():
     img = os.path.join(app.config['UPLOAD_FOLDER'], 'shoes.png')
-    print(img)
-    return render_template("home.html", user_image=img)
+    img2 = os.path.join(app.config['UPLOAD_FOLDER'], 'ladies.png')
+    img3 = os.path.join(app.config['UPLOAD_FOLDER'], 'mens.png')
+    img4 = os.path.join(app.config['UPLOAD_FOLDER'], 'kids.png')
+    promo = os.path.join(app.config['UPLOAD_FOLDER'], 'promotion.jpg')
+    return render_template("home.html", user_image=img,user_image1=img2,user_image2=img3,user_image3=img4, promo=promo)
 
+@app.route('/CreateMember', methods=['GET','POST'])
+def create_user():
+    create_user_form = CreateUserForm(request.form)
+    if request.method == 'POST' and create_user_form.validate():
+        users_dict = {}
+        db = shelve.open('storage.db', 'c')
+        try:
+            users_dict = db['Users']
+        except:
+            print("Error in retrieving Users from storage.db.")
+        user = member.member(create_user_form.first_name.data,
+                         create_user_form.last_name.data)
+        users_dict[user.get_user_id()] = user
+        db['Users'] = users_dict
+        # Test codes
+        users_dict = db['Users']
+        user = users_dict[user.get_user_id()]
+        print(user.get_first_name(), user.get_last_name(), "was stored in storage.db successfully with user_id == ",user.get_user_id())
+        db.close()
+        return redirect(url_for('retrieve_users'))
+    return render_template('CreateMember.html', form=create_user_form)
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 
 
---
+++ Azrul/form.py
@@ -0,0 +1,15 @@
+from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators, EmailField,TelField, DateField
+import wtforms.fields as fld
+class CreateUserForm(Form):
+    first_name = StringField("First Name",[validators.Length(min=1,max=30),validators.data_required()])
+    last_name = StringField("Last Name", [validators.Length(min=1, max=30), validators.data_required()])
+    email = EmailField("Email",[validators.data_required()])
+    new_password = fld.PasswordField()
+    confirm_password = fld.PasswordField(validators=[validators.EqualTo("new_password")])
+    dob = DateField('Date',format='%Y-%m-%d')
+    gender = SelectField('Gender', [validators.DataRequired()],choices=[('','Select'),('F','Female'),('M','Male')],default='')
+    phonenumber = TelField("Phone Number",[validators.Length(min=8,max=8), validators.data_required()])
+
+class loginpage():
+    phonenumber = TelField("Phone Number",[validators.Length(min=8,max=8), validators.data_required()])
+    passwrd = fld.PasswordField(validators=[validators.InputRequired()])
